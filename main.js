78d82a64c1a438317ef06137631608429ade3fd7
Jim Vickery <jimrv8@yahoo.com>
2017-07-27 09:30:19 -0500

    updated main.js and index.html file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('in main.js');
 
 //get conversion placeholder
-let conversionEl = document.getElementById("conversion");
+// let conversionEl = document.getElementById("conversion");
 
 //get words from input field
 var originalText = document.getElementById("get-words") ;
@@ -12,9 +12,8 @@ var originalText = document.getElementById("get-words") ;
 var button = document.getElementById("btn-translate");
 button.addEventListener("click", translateButton); 
 
-gaelicEl = document.getElementById("gaelic");
-yiddishEL = document.getElementById("yiddish");
-spanishEL = document.getElementById("spanish");
+var outputEL = document.getElementById("converted");
+
 
 
 function translateButton() {
@@ -23,86 +22,54 @@ function translateButton() {
     var gaelicArray = [];
     var yiddishArray = [];
     var spanishArray = [];
+    var kurdishArray = [];
 
     for (var i = 0; i < originalTextArray.length; i++) {
         gaelicArray.push(FuzzyDuck.translateToGaelic(originalTextArray[i]));
         yiddishArray.push(FuzzyDuck.translateToYiddish(originalTextArray[i]));
         spanishArray.push(FuzzyDuck.translateToSpanish(originalTextArray[i]));
+        kurdishArray.push(FuzzyDuck.translateToKurdish(originalTextArray[i]));
         
     }
 
     var resultGaelic = convertArrayToPhrase(gaelicArray);
     var resultYiddish = convertArrayToPhrase(yiddishArray);
     var resultSpanish = convertArrayToPhrase(spanishArray);
+    var resultKurdish = convertArrayToPhrase(kurdishArray);
+   
+    
+    
+   
+    let whichRadioselected; 
+    var radios = document.getElementsByName("whichLang");
+    for (let i = 0; i < radios.length; i++) {
+        //0 = gaelic, 1 = yiddish, 2 = spanish 3 = teds language
+        if (radios[i].checked) {
+            whichRadioselected = radios[i].value;
+            break;
+
+        } 
+    }
+    console.log('output', outputEL);
+    
+    if (whichRadioselected == 0) {
     
     //set to an output --- add output element 
-    gaelicEl.innerHTML = "The result in Gaelic " + resultGaelic;
-    yiddishEL.innerHTML = "The result in Yiddish " + resultYiddish; 
-    spanishEL.innerHTML = "The result in Spanish " + resultSpanish;
-   
+    outputEL.innerHTML = "The result in Gaelic:  " + resultGaelic;
+        
+    }else if (whichRadioselected == 1) {
+       //set to an output --- add output element 
+        outputEL.innerHTML = "The result in Yiddish:  " + resultYiddish; 
+
+
+    }else if (whichRadioselected == 2) {
+        //set to an output --- add output element 
+        outputEL.innerHTML = "The result in Spanish: " + resultSpanish;
+    }else if (whichRadioselected == 3) {
+        //set to an output --- add output element 
+        outputEL.innerHTML = "The result in Kurdish: " + resultKurdish;
+    }
+
+
 }
-
-// translateButton();
-
-
-//get placeholder for original message
-//  var originalMessageEl = document.getElementById("original-text")
-
-
-//check holiday array
-// let holidayButton = document.getElementById("checkHoliday");
-// holidayButton.addEventListener("click", introduceHolidays);
-
-// function  introduceHolidays(event){
-//     console.log("Holiday IIFE", Fuzzyduck);
-// }
-
-// start
-// let runLanguageBtn = document.getElementById("btn-translate");
-// button.addEventListener("click", getRadio);
-
-// function getRadio () {
-//     let langTextInput = document.getElementById("get-words").value;
-//     let whichRadioselected; 
-//     var radios = document.getElementsByName("whichside");
-//     for (let i = 0; i < radios.length; i++) {
-//         //0 = gaelic, 1 = yiddish, 2 = spanish 3 = teds language
-//         if (radios[i].checked) {
-//             whichRadioselected = radios[i].value;
-//             break;
-
-//         } 
-//     }
-//     // console.log('whichRadioSelected', whichRadioselected);
-    
-//     if (whichRadioselected == 0) {
-//          //FuzzyDuck.method goes here
-//         // console.log("you selected Gaelic");
-//         // var convertedMessage = "Gaelic: ";
-//         conversionEl.innerHTML = resultGaelic;
-        
-//     }else if (whichRadioselected == 1) {
-//         //FuzzyDuck.method goes here
-//         // console.log("you selected Yiddish");
-//         //  var convertedMessage = "Gaelic: ";
-//         conversionEl.innerHTML = resultYiddish;
-
-
-//     }else if (whichRadioselected == 2) {
-//          //FuzzyDuck.method goes here
-//         // console.log("you selected Spanish");
-//          conversionEl.innerHTML = resultSpanish;
-//         //  var convertedMessage = "Spanish: ";
-//         //  conversionEl.innerHTML = resultSpanish;
-//     }else if (whichRadioselected == 3) {
-//          //FuzzyDuck.method goes here
-//         // console.log("you selected Teds Language");
-        
-//     }
-
-//     //display original message in div container original-text
-//     // var originalMessage = document.getElementById("get-words").value;
-//     // originalMessageEl.innerHTML =     "<pre>" + "English:  " + originalMessage  + "\n\n" + convertedMessage + "</pre>";
-    
-// }
 
